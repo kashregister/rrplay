ad5bacc47d7d76a56389c5240031b000ff40f1ca
kashregister <lan.hodzic@gmail.com>
2025-03-29 22:08:24 +0100

    choose your path with a config

--- src/player.rs
+++ src/player.rs
@@ -16,16 +16,15 @@ pub struct Player {
 
 impl Player {
     pub fn init() -> Player {
-        let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
         let entry = SongEntry {
             file: PathBuf::new(),
             score: 0,
         };
-        return Player {
+        Player {
             current_song: entry,
             playing: Arc::new(Mutex::new(true)),
             skip: Arc::new(Mutex::new(false)),
-        };
+        }
     }
     pub fn play_song(&mut self) {
         let file = BufReader::new(File::open(&self.current_song.file).unwrap());
@@ -33,7 +32,7 @@ impl Player {
         let playing = self.playing.clone();
 
         let skip = self.skip.clone();
-        let handle = thread::spawn(move || {
+        thread::spawn(move || {
             let (_stream, stream_handle) = OutputStream::try_default().unwrap();
             let sink = Sink::try_new(&stream_handle).unwrap();
 
@@ -64,11 +63,7 @@ impl Player {
 
     pub fn is_playing(&mut self) -> bool {
         let playing_guard = self.playing.lock().unwrap();
-        if playing_guard.eq(&true) {
-            return true;
-        } else {
-            return false;
-        }
+        playing_guard.eq(&true)
     }
     pub fn pause_song(&mut self) {
         let mut playing_guard = self.playing.lock().unwrap();
