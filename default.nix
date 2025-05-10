2919c4cfe3814d9b4a7c4586ada9bdcbd6ed851b
kashregister <58663785+kashregister@users.noreply.github.com>
2025-05-11 00:21:45 +0200

    changed flake

---
+++ default.nix
@@ -0,0 +1,20 @@
+{pkgs ? import <nixpkgs> {}}: let
+  manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+in
+  pkgs.rustPlatform.buildRustPackage rec {
+    pname = manifest.name;
+    version = manifest.version;
+    cargoLock.lockFile = ./Cargo.lock;
+    src = pkgs.lib.cleanSource ./.;
+
+    buildInputs = with pkgs; [
+      dbus
+      alsa-lib
+    ];
+
+    nativeBuildInputs = with pkgs; [
+      pkg-config
+    ];
+
+    dbus = pkgs.dbus;
+  }
