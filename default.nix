bd0fec88f6f308c7f05b4acac25e143ab0a38841
kashregister <58663785+kashregister@users.noreply.github.com>
2025-05-11 13:36:28 +0200

    updated default.nix

--- default.nix
+++ default.nix
@@ -1,11 +1,15 @@
-{pkgs ? import <nixpkgs> {}, ...}: let
+{
+  pkgs ? import <nixpkgs> {},
+  src ? ./.,
+}: let
+  theSource = src;
   manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
 in
   pkgs.rustPlatform.buildRustPackage rec {
     pname = manifest.name;
     version = manifest.version;
-    cargoLock.lockFile = ./Cargo.lock;
-    src = pkgs.lib.cleanSource ./.;
+    cargoLock.lockFile = "${src}/Cargo.lock";
+    src = pkgs.lib.cleanSource theSource;
 
     buildInputs = with pkgs; [
       dbus
